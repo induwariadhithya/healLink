5dd7d1d5578cd1e998658b58b93185939a67ad4d
induwariadhithya <adithya20030615@gmail.com>
2026-09-05 15:15:25 +0530

    Support singular journal API route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/journals", journalRoutes);
+app.use("/api/journal", journalRoutes);
 app.use("/api/moods", moodRoutes);
 
 const startServer = async () => {
