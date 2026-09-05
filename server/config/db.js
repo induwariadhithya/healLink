dbb18ce8baf737d3a1f9a1b00f7bf26ba066ecca
YasuriNethmini <nethminiabeywickrama531@gmail.com>
2026-09-05 13:41:29 +0530

    Validate MongoDB environment configuration

--- server/config/db.js
+++ server/config/db.js
@@ -10,6 +10,10 @@ dns.setServers(dnsServers);
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing from server/.env");
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
